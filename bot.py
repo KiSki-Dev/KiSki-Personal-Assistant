96bdb2efcf62c7125f4973dfda7b32f2748505be
KiSki-Dev <106085521+KiSki-Dev@users.noreply.github.com>
2023-12-02 18:18:17 +0100

    Added 'Ping' and 'Echo' Command

--- bot.py
+++ bot.py
@@ -2,19 +2,34 @@ import os
 import discord
 from discord.ext import commands
 from discord.utils import get
+from discord import app_commands
+
 from dotenv import load_dotenv
 
-env_file_path = "C:\\Users\\kille\\Desktop\\KiSki Personal Assistant/.env"
+env_file_path = r"C:\Users\kille\Desktop\KiSki Personal Assistant\KiSki-Personal-Assistant\.env"
 load_dotenv(dotenv_path=env_file_path)
 
 token = str(os.getenv("TOKEN"))
 
-client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-client.remove_command('help')
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+tree = app_commands.CommandTree(client)
+guildID = 1171187810162716673
+
 
 @client.event
 async def on_ready():
-    os.system("cls")
-    print("\Bot is online!\n")
+    await tree.sync(guild=discord.Object(id=guildID))
+    print("Bot is online!")
+
+@tree.command(name="ping", description = "Check the currents Bot Ping", guild=discord.Object(id=guildID))
+async def first_command(interaction,):
+    await interaction.response.send_message('Pong! My current Ping is: {0}'.format(round(client.latency, 5)))
+
+@tree.command(name="echo", description = "Repeats your Message", guild=discord.Object(id=guildID))
+async def first_command(interaction, message: str):
+    await interaction.response.send_message(message)
+
+
 
 client.run(token)
